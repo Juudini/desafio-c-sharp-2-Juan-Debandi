af6fe24a9ce2eca82d52090d5e2d372dfe82a295
Juudini <juudinidev@gmail.com>
2024-02-05 21:52:45 -0300

    first commit

---
+++ ProductoVendido.cs
@@ -0,0 +1,55 @@
+﻿namespace Desafio2
+{
+    public class ProductoVendido
+    {
+        private int _id;
+        private int _idProducto;
+        private int _stock;
+        private int _idVenta;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int idProducto
+        {
+            get { return _idProducto; }
+            set { _idProducto = value; }
+        }
+
+        public int stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+
+        public int idVenta
+        {
+            get { return _idVenta; }
+            set { _idVenta = value; }
+        }
+
+        public ProductoVendido()
+        {
+            _id = 0;
+            _idProducto = 0;
+            _stock = 0;
+            _idVenta = 0;
+        }
+
+        public ProductoVendido(int id, int idProducto, int stock, int idVenta)
+        {
+            this._id = id;
+            this._idProducto = idProducto;
+            this._stock = stock;
+            this._idVenta = idVenta;
+        }
+
+
+
+
+    }
+}
+
---
+++ Usuario.cs
@@ -0,0 +1,106 @@
+﻿namespace Desafio2
+{
+    public class Usuario
+    {
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+        private string _nombreUsuario;
+        private string _contraseña;
+        private string _mail;
+
+        public int id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+        public string apellido
+        {
+            get
+            {
+                return _apellido;
+            }
+            set
+            {
+                _apellido = value;
+            }
+        }
+
+        public string nombreusuario
+        {
+            get
+            {
+                return _nombreUsuario;
+            }
+            set
+            {
+                _nombreUsuario = value;
+            }
+        }
+
+        public string contraseña
+        {
+            get
+            {
+                return _contraseña;
+            }
+            set
+            {
+                _contraseña = value;
+            }
+        }
+
+        public string mail
+        {
+            get
+            {
+                return _mail;
+            }
+            set
+            {
+                _mail = value;
+            }
+        }
+
+
+        //Inicializo
+        public Usuario()
+        {
+            _id = 0;
+            _nombre = string.Empty;
+            _apellido = string.Empty;
+            _nombreUsuario = string.Empty;
+            _contraseña = string.Empty;
+            _mail = string.Empty;
+        }
+
+        //constructor
+        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contraseña, string mail)
+        {
+            this._id = id;
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._nombreUsuario = nombreUsuario;
+            this._contraseña = contraseña;
+            this._mail = mail;
+        }
+    }
+}
